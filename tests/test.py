5263bf3c929d058fb57231b32c61d05708b271ad
linporu <qazwsx123456guy@gmail.com>
2024-11-03 10:01:08 +0800

    test: add test case - add test case to format_ts_datetime

--- tests/test.py
+++ tests/test.py
@@ -10,3 +10,13 @@ from helper import *
 def test_format_ts_YYMMDD_expected(valid_input, expected):
 
 	assert format_ts_YYMMDD(valid_input) == expected
+
+
+@pytest.mark.parametrize("valid_input, expected", [
+
+("1234-11-02T22:56:59Z", "1234-11-02 22:56"),
+
+])
+def test_format_ts_datetime_expected(valid_input, expected):
+
+	assert format_ts_datetime(valid_input) == expected
